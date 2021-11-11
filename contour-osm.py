3f11b87cd7380178c3aa6af1a16a3b58801359f0
roelderickx <roel.derickx@gmail.com>
2021-11-11 14:23:23 +0100

    Use separate logger for contour-osm and ogr2osm, set proper logging level for ogr2osm

--- contour-osm.py
+++ contour-osm.py
@@ -16,20 +16,22 @@
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
-import sys, os, argparse, logging, ogr2osm
+import sys
+import os
+import argparse
+import logging
+import ogr2osm
 from xml.dom import minidom
 from osgeo import ogr
 from osgeo import osr
-
-logging.basicConfig(format = '%(message)s', level = logging.INFO)
 
 class Polyfile:
     def __init__(self):
         self.__filename = None
         self.__name = None
         self.__polygons = ogr.Geometry(ogr.wkbMultiPolygon)
-    
-    
+
+
     def set_boundaries(self, minlon, maxlon, minlat, maxlat):
         self.__name = 'bbox'
         polygon = ogr.Geometry(ogr.wkbPolygon)
@@ -42,15 +44,15 @@ class Polyfile:
         poly_section.AddPoint(minlon, minlat)
         polygon.AddGeometry(poly_section)
         self.__polygons.AddGeometry(polygon)
-    
-    
+
+
     def read_file(self, filename):
         self.__filename = filename
         self.__polygons = ogr.Geometry(ogr.wkbMultiPolygon)
-        
+
         self.__read_poly()
-    
-    
+
+
     def __read_poly(self):
         with open(self.__filename, 'r') as f:
             self.__name = f.readline().strip()
@@ -71,8 +73,8 @@ class Polyfile:
                     polygon.AddGeometry(self.__read_poly_section(f))
                     self.__polygons.AddGeometry(polygon)
         f.close()
-    
-    
+
+
     def __read_poly_section(self, f):
         poly_section = ogr.Geometry(ogr.wkbLinearRing)
         while True:
@@ -87,13 +89,13 @@ class Polyfile:
                 ords = line.split()
                 # add point in traditional GIS order (east, north)
                 poly_section.AddPoint(float(ords[0]), float(ords[1]))
-                
+
         return poly_section
 
 
     def get_geometry(self, srs = 4326):
         polygons = self.__polygons.Clone()
-        
+
         src_spatial_ref = osr.SpatialReference()
         src_spatial_ref.SetAxisMappingStrategy(osr.OAMS_TRADITIONAL_GIS_ORDER)
         src_spatial_ref.ImportFromEPSG(4326)
@@ -104,9 +106,9 @@ class Polyfile:
 
         transform = osr.CoordinateTransformation(src_spatial_ref, dest_spatial_ref)
         polygons.Transform(transform)
-        
+
         return polygons
-    
+
     '''
     def to_wkt_string(self, srs = 4326, force_eastnorth_orientation = False):
         return self.get_geometry(srs, force_eastnorth_orientation).ExportToWkt()
@@ -121,6 +123,8 @@ class Polyfile:
 
 class ContourTranslation(ogr2osm.TranslationBase):
     def __init__(self, is_database_source, src_srs, poly, major_category, medium_category):
+        self.logger = logging.getLogger('contour-osm')
+
         self.is_database_source = is_database_source
         self.src_srs = src_srs
         self.boundaries = None
@@ -128,42 +132,42 @@ class ContourTranslation(ogr2osm.TranslationBase):
             self.boundaries = poly.get_geometry(src_srs)
         self.major_category = major_category
         self.medium_category = medium_category
-        
+
         # intersection datasource, should not go out of scope as long as the intersection layer is used
         self.intersect_ds = None
-    
-    
+
+
     def filter_layer(self, layer):
         if self.is_database_source or not self.boundaries:
             return layer
-        
+
         spatial_ref = osr.SpatialReference()
         spatial_ref.SetAxisMappingStrategy(osr.OAMS_TRADITIONAL_GIS_ORDER)
         spatial_ref.ImportFromEPSG(self.src_srs)
-        
+
         dst_driver = ogr.GetDriverByName('MEMORY')
         self.intersect_ds = dst_driver.CreateDataSource('memdata')
         dst_layer = self.intersect_ds.CreateLayer(layer.GetName(), \
                                                   srs = spatial_ref, \
                                                   geom_type = ogr.wkbMultiLineString)
         dst_layer_def = dst_layer.GetLayerDefn()
-        
+
         # add input layer field definitions to the output layer
         src_layer_def = layer.GetLayerDefn()
         for i in range(src_layer_def.GetFieldCount()):
             src_field_def = src_layer_def.GetFieldDefn(i)
             dst_layer.CreateField(src_field_def)
-        
+
         # add intersection of input layer features with input geometry to output layer
         amount_intersections = 0
         for j in range(layer.GetFeatureCount()):
             src_feature = layer.GetNextFeature()
             src_geometry = src_feature.GetGeometryRef()
-            
+
             if self.boundaries.Intersects(src_geometry):
                 amount_intersections += 1
                 intersection = self.boundaries.Intersection(src_geometry)
-                
+
                 dst_feature = ogr.Feature(dst_layer_def)
                 for k in range(dst_layer_def.GetFieldCount()):
                     dst_field = dst_layer_def.GetFieldDefn(k)
@@ -171,23 +175,23 @@ class ContourTranslation(ogr2osm.TranslationBase):
                 dst_feature.SetGeometry(intersection)
 
                 dst_layer.CreateFeature(dst_feature)
-        
-        logging.info('Amount of features in source: %d' % layer.GetFeatureCount())
-        logging.info('Amount of intersections found: %d' % amount_intersections)
-        
+
+        self.logger.info('Amount of features in source: %d', layer.GetFeatureCount())
+        self.logger.info('Amount of intersections found: %d', amount_intersections)
+
         return dst_layer
-    
-    
+
+
     def filter_tags(self, attrs):
         if not attrs:
             return
-        
+
         tags={}
-        
+
         if 'height' in attrs:
             tags['ele'] = attrs['height']
             tags['contour'] = 'elevation'
-            
+
             height = int(float(attrs['height']))
             if height % self.major_category == 0:
                 tags['contour_ext'] = 'elevation_major'
@@ -195,7 +199,7 @@ class ContourTranslation(ogr2osm.TranslationBase):
                 tags['contour_ext'] = 'elevation_medium'
             else:
                 tags['contour_ext'] = 'elevation_minor'
-        
+
         return tags
 
 
@@ -241,28 +245,30 @@ def parse_commandline():
 
 
 def examine_layer(datasource, preferred_tablename, preferred_heightcol, preferred_contourcol):
-    logging.info("Get layer features")
+    logger = logging.getLogger('contour-osm')
+
+    logger.info("Get layer features")
 
     tablename = preferred_tablename
     heightcol = preferred_heightcol
     contourcol = preferred_contourcol
-    
+
     layer = None
     if not preferred_tablename:
         if datasource.get_layer_count() > 0:
             (layer, reproject) = datasource.get_layer(0)
             tablename = layer.GetName()
-            
+
             if datasource.get_layer_count() > 1:
-                logging.warning("More than one layer found.")
+                logger.warning("More than one layer found.")
         else:
-            logging.error("No layer found and none was given.")
+            logger.error("No layer found and none was given.")
     else:
         layer = datasource.datasource.GetLayer(preferred_tablename)
-    logging.info("Using layer %s" % tablename)
+    logger.info("Using layer %s", tablename)
 
     layerdef = layer.GetLayerDefn()
-    
+
     features_without_id = \
         [ layerdef.GetFieldDefn(i).GetName() \
                 for i in range(layerdef.GetFieldCount()) \
@@ -271,31 +277,33 @@ def examine_layer(datasource, preferred_tablename, preferred_heightcol, preferre
     if preferred_heightcol and preferred_heightcol in features_without_id:
         heightcol = preferred_heightcol
     elif len(features_without_id) == 0:
-        logging.error("No feature found and none was given.")
+        logger.error("No feature found and none was given.")
     else:
         heightcol = features_without_id[0]
         if len(features_without_id) > 1:
-            logging.warning("More than one feature found.")
-    logging.info("Using feature %s" % heightcol)
-    
+            logger.warning("More than one feature found.")
+    logger.info("Using feature %s", heightcol)
+
     geoms = [ layerdef.GetGeomFieldDefn(i).GetName() \
                     for i in range(layerdef.GetGeomFieldCount()) ]
     if preferred_contourcol and preferred_contourcol in geoms:
         contourcol = preferred_contourcol
     elif len(geoms) == 0:
-        logging.error("No geometry found and none was given.")
+        logger.error("No geometry found and none was given.")
     else:
         contourcol = geoms[0]
         if len(geoms) > 1:
-            logging.warning("More than one geometry found.")
-    logging.info("Using geometry %s" % contourcol)
-    
+            logger.warning("More than one geometry found.")
+    logger.info("Using geometry %s", contourcol)
+
     return (tablename, heightcol, contourcol)
 
 
 def get_query(tablename, heightcolumn, geomcolumn, boundaries, src_srs):
+    logger = logging.getLogger('contour-osm')
+
     sql = ''
-    
+
     if boundaries:
         sql = '''select %s, ST_Intersection(%s, p.polyline)
 from (select ST_GeomFromText(\'%s\', %d)  polyline) p, %s
@@ -307,12 +315,20 @@ where ST_Intersects(%s, p.polyline)''' % \
         sql = 'select %s, %s from %s' % \
             (heightcolumn, geomcolumn, tablename)
 
-    logging.info('Generated SQL statement: %s' % sql)
-    
+    logger.info('Generated SQL statement: %s', sql)
+
     return sql
 
 
 def main():
+    logger = logging.getLogger('contour-osm')
+    logger.setLevel(logging.DEBUG)
+    logger.addHandler(logging.StreamHandler())
+
+    ogr2osmlogger = logging.getLogger('ogr2osm')
+    ogr2osmlogger.setLevel(logging.ERROR)
+    ogr2osmlogger.addHandler(logging.StreamHandler())
+
     params = parse_commandline()
 
     poly = None
